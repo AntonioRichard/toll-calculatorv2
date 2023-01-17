e1035ed99bcf9cc5db0eef5eabd64dbd053b7e39
DESKTOP-CDG0GBS\Toni <antonio.richard01@e-uvt.ro>
2023-01-17 13:46:34 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -4,11 +4,18 @@ const app = express();
 const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log("Server is up");
